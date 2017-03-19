b92d88b20c8ef358fc4d645586d28f4262a41ba7
shopsmartin <shopsmartin@gmail.com>
2017-03-19 10:59:42 -0500

    another

--- programming_assignment3.R
+++ programming_assignment3.R
@@ -24,16 +24,49 @@ best <- function(state, outcomeName) {
         outcome <- read.csv("outcome-of-care-measures.csv", colClasses = "character")[, c(2, 7, cols[outcomeName])]
         names(outcome) <- c("hosp", "state", "rate")
         outcome[outcome == "Not Available"] = NA
-        outcome <- outcome[order(outcome$rate,outcome$hosp),]
+        outcome$rate = as.numeric(outcome$rate)
+        outcome <- split(outcome, outcome$state)
+        outcome <- outcome[state][[1]]
+        outcome <- outcome[order(outcome$rate, outcome$hosp),]
     }
     outcome$hosp[1]
 }
 
+rankhospital <- function(state, outcomeName, num="best") {
+    checkarg <- function(state) {
+        state %in% state.abb
+    }
+    cols <- c(11, 17, 23)
+    names(cols) <- c("heart attack", "heart failure", "pneumonia")
+    outcomeName <- tolower(outcomeName)
+    if (!checkarg(state)) { 
+        stop("Invalid State")
+    }
+    else if (!is.element(outcomeName, c("heart attack", "pneumonia", "heart failure"))) { 
+        stop("invalid outcome")
+    }
+    else if (num == "best") { 
+        return (best(state = state, outcomeName = outcomeName))
+    }
+    else if (num == "worst") {
+        outcome <- read.csv("outcome-of-care-measures.csv", colClasses = "character")[, c(2, 7, cols[outcomeName])]
+        names(outcome) <- c("hosp", "state", "rate")
+        outcome[outcome == "Not Available"] = NA
+        outcome$rate = as.numeric(outcome$rate)
+        outcome <- split(outcome, outcome$state)
+        outcome <- outcome[state][[1]]
+        outcome <- outcome[order( - outcome$rate, outcome$hosp),]
+        return(outcome$hosp[1])
+    }
+    else {
+        outcome <- read.csv("outcome-of-care-measures.csv", colClasses = "character")[, c(2, 7, cols[outcomeName])]
+        names(outcome) <- c("hosp", "state", "rate")
+        outcome[outcome == "Not Available"] = NA
+        outcome$rate = as.numeric(outcome$rate)
+        outcome <- split(outcome, outcome$state)
+        outcome <- outcome[state][[1]]
+        outcome <- outcome[order(outcome$rate, outcome$hosp),]
+    }
+    outcome$hosp[num]
+}
 
-
-
-
-
-[23] "Hospital.30.Day.Death..Mortality..Rates.from.Pneumonia"
-[17] "Hospital.30.Day.Death..Mortality..Rates.from.Heart.Failure"
-11"Hospital.30.Day.Death..Mortality..Rates.from.Heart.Attack"
